7a27d33a4d4bb3d0782922edb0ffcf3e749b197c
John <jeden667@gmail.com>
2022-08-29 14:25:35 +0400

    add Proj-004_DarlingGreeting

---
+++ Proj-004_DarlingGreeting/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Enter your name: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "john")
+{
+    Console.WriteLine("Hey Johnny buddy! How are you?");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
+Console.WriteLine(" ");
