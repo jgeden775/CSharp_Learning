2686393f304e0d40c288b26382d4999c3f5be349
John <jeden667@gmail.com>
2022-08-30 20:54:37 +0400

    fixes in Proj-002_UserGreeting

--- Proj-002_UserGreeting/Program.cs
+++ Proj-002_UserGreeting/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.Write("Enter your name: ");
+﻿Console.Clear();
+Console.Write("Enter your name: ");
 string username = Console.ReadLine();
 Console.Write("Hello, ");
 Console.WriteLine(username);
