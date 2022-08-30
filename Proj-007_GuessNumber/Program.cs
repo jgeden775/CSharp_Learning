6343844bea2d36c9994ad1a46e24ced45bd2ce13
John <jeden667@gmail.com>
2022-08-30 22:59:22 +0400

    add Proj-007_GuessNumber

---
+++ Proj-007_GuessNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число от 1 до 10: ");
+string name = Console.ReadLine();
+int num = Convert.ToInt32(name);
+int secnum = 7;
+if (num > secnum)
+{
+    Console.WriteLine("Секретное число меньше.");
+}
+else if (num < secnum)
+{
+    Console.WriteLine("Секретное число больше.");
+}
+else
+{
+    Console.WriteLine("Ура, вы угадали!");
+}
+Console.WriteLine(" ");
