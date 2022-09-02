b430e0a9767f929affb0aedfb72ada9350bfe46b
John <jeden667@gmail.com>
2022-09-02 15:16:17 +0400

    add Proj-008_MethodFx

---
+++ Proj-008_MethodFx/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.WriteLine("<< Функция f(x) = x * x + 1 >>");
+Console.WriteLine();
+Console.WriteLine("Введите целое число x: ");
+string name = Console.ReadLine();
+int x = Convert.ToInt32(name);
+
+int Fx(int arg)
+{
+    int result = arg * arg + 1;
+    return result;
+}
+
+int fxnum = Fx(x);
+
+Console.WriteLine();
+Console.WriteLine($"Результат: {fxnum}");
+Console.WriteLine();
