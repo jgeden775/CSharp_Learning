9efff363a4d272d78fb3e3898509875d42b198e1
John <jeden667@gmail.com>
2022-09-02 15:49:34 +0400

    add Proj-009_FindArrayN

---
+++ Proj-009_FindArrayN/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+// Нахождение индекса элемента массива
+int[] array = {5, 12, 44, 6, 1, 7, 13, 6, 93, 9, 17}; // Некоторый массив
+int n = array.Length;
+int find = 6; // Число, индекс которого ищем
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
